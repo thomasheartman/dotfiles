5dd89f3fd0119282e9e3d89a70991938eff5ab39
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-07-11 09:34:00 +0200

    Adds pl as alias for pijul

--- .aliases/aliases.windows.bat
+++ .aliases/aliases.windows.bat
@@ -3,3 +3,5 @@
 DOSKEY code=start /b cmd /c "C:\Program Files\Microsoft VS Code Insiders\Code - Insiders.exe" $*
 
 DOSKEY dot="C:\Program Files\Git\cmd\git.EXE" --git-dir=%HOME%\dotfiles\ --work-tree=%HOME% $*
+
+DOSKEY pl="pijul" $*
