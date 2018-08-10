1fccd6932349095aced993c8498f2242164a5557
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-10 15:47:30 +0200

    Changes code to be an alias of code-insiders

--- .aliases/aliases.windows.bat
+++ .aliases/aliases.windows.bat
@@ -1,6 +1,6 @@
 @echo off
 
-DOSKEY code=start /b cmd /c "C:\Program Files\Microsoft VS Code Insiders\Code - Insiders.exe" $*
+DOSKEY code=start /b cmd /c "code-insiders" $*
 
 DOSKEY dot="C:\Program Files\Git\cmd\git.EXE" --git-dir=%HOME%\dotfiles\ --work-tree=%HOME% $*
 
