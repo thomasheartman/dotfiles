de42a8dc3543df8d246f84cfe769d6a125d58f62
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-10-27 20:51:52 +0200

    Adds editor and path vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,9 @@
 source ~/.aliases/aliases.macos.sh
 source ~/.aliases/aliases.shared.zsh
 
-set $EDITOR="vim"
-set $VISUAL=$EDITOR
-set PATH $HOME/.cargo/bin $PATH
+set -gx EDITOR vim
+set -gx VISUAL $EDITOR
+set PATH $HOME/.cargo/bin ./node_modules $PATH
 
 function fish_user_key_bindings
   fish_vi_key_bindings
