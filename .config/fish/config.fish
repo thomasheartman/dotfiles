673d9299a6a02c99f3ba844b2e3214790a593298
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-11-04 09:20:17 +0100

    Adds global yarn bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ source ~/.aliases/aliases.fish
 
 set -gx EDITOR vim
 set -gx VISUAL $EDITOR
-set PATH $HOME/.cargo/bin ./node_modules $PATH
+set PATH $HOME/.cargo/bin $HOME/.yarn/bin ./node_modules $PATH
 
 function fish_user_key_bindings
   fish_vi_key_bindings
