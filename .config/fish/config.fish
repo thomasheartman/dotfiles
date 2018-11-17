68174d5788e9d9cd94ad6875409a209479da0e5b
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-11-17 06:47:57 +0100

    Adds ssh-agent snippet

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,42 @@ set -g fish_key_bindings fish_user_key_bindings
 if test -e ~/.nix-profile/etc/profile.d/nix.sh; and type -q bass
     bass source ~/.nix-profile/etc/profile.d/nix.sh
 end
+
+# ssh
+setenv SSH_ENV $HOME/.ssh/environment
+
+function start_agent
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "Succeeded!"
+    chmod 600 $SSH_ENV
+    . $SSH_ENV > /dev/null
+    ssh-add
+end
+
+function test_identities
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
+    end
+end
+
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end
+else
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV > /dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else
+        start_agent
+    end
+end
