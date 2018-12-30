fc0f9a56f360ad7a57f0191f18c4b43bdf6d01f8
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-12-30 11:00:08 +0100

    Adds /dev/null redirection to avoid polluting startup prompt

--- .config/fish/functions/em.fish
+++ .config/fish/functions/em.fish
@@ -1,5 +1,5 @@
 function em
-    if emacsclient -ne "(if (> (length (frame-list)) 1) 't)" | grep -q t
+    if emacsclient -ne "(if (> (length (frame-list)) 1) 't)" | grep -q t > /dev/null
         emacsclient -nq $argv
     else
         emacsclient -a '' -nqc $argv
