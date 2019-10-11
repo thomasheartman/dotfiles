8772a77da611d36c93dcd1f08c23b394f290e03c
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2019-10-11 18:39:22 +0200

    adds nixfmt and formats file

--- default.nix
+++ default.nix
@@ -1,52 +1,50 @@
-/*
-Grabbed from here: https://gist.github.com/lheckemann/402e61e8e53f136f239ecd8c17ab1deb
+/* Grabbed from here: https://gist.github.com/lheckemann/402e61e8e53f136f239ecd8c17ab1deb
 
 Save this file to ~/default.nix or your preferred path (make sure to
 change the location in the update-profile script if you choose a
 different place).
 Install using `nix-env -f ~ --set`, from then on use `update-profile`.
 */
-{ pkgs ? import <nixpkgs> {}
-, name ? "user-env"
-}: with pkgs;
+{ pkgs ? import <nixpkgs> { }, name ? "user-env" }:
+with pkgs;
 let wally-cli = import ./wally-cli.nix;
-in
-buildEnv {
+in buildEnv {
   inherit name;
-  extraOutputsToInstall = ["out" "bin" "lib"];
+  extraOutputsToInstall = [ "out" "bin" "lib" ];
   paths = with xfce; [
-        alacritty
-        cargo
-        chromium
-        direnv
-        docker
-        emacs
-        ffmpeg
-        firefox
-        haskellPackages.apply-refact
-        haskellPackages.hlint
-        haskellPackages.hindent
-        haskellPackages.stylish-haskell
-        haskellPackages.hasktags
-        haskellPackages.hoogle
-        ispell
-        libusb
-        moka-icon-theme
-        mpv
-        mu
-        nixfmt
-        pavucontrol
-        pandoc
-        pijul
-        powertop
-        ripgrep
-        slack
-        spotify
-        stack
-        teensy-loader-cli
-        vlc
-        wally-cli
-        xfce.xfwm4-themes
+    alacritty
+    cargo
+    chromium
+    direnv
+    docker
+    emacs
+    ffmpeg
+    firefox
+    haskellPackages.apply-refact
+    haskellPackages.hlint
+    haskellPackages.hindent
+    haskellPackages.stylish-haskell
+    haskellPackages.hasktags
+    haskellPackages.hoogle
+    ispell
+    libusb
+    moka-icon-theme
+    mpv
+    mu
+    nixfmt
+    pavucontrol
+    pandoc
+    pijul
+    powertop
+    ripgrep
+    slack
+    spotify
+    stack
+    teensy-loader-cli
+    tmux
+    vlc
+    wally-cli
+    xfce.xfwm4-themes
 
     (writeScriptBin "update-profile" ''
       #!${stdenv.shell}
@@ -58,12 +56,12 @@ buildEnv {
       name = "break-nix-env-manifest";
       destination = "/manifest.nix";
       text = ''
-        throw ''\''
+        throw '''
           Your user environment is a buildEnv which is incompatible with
           nix-env's built-in env builder. Edit your home expression and run
           update-profile instead!
-        ''\''
-      '';
+        '''
+        '';
     })
     # To allow easily seeing which nixpkgs version the profile was built from, place the version string in ~/.nix-profile/nixpkgs-version
     (writeTextFile {
