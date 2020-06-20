7af9d2e8dfa04549acd967433cb8c505002675b5
Thomas Heartman <thomas.heartman+git@gmail.com>
2020-06-20 20:14:44 +0200

    add 'e' alias for emacsclient

--- .aliases/aliases.fish
+++ .aliases/aliases.fish
@@ -1,5 +1,6 @@
 alias vi="vim" # to get the good kind of vim
 alias emt="emacsclient -t"
+alias e="emacsclient"
 alias dot="git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
 alias windot="/usr/bin/git --git-dir=/mnt/c/Users/thomas/dotfiles/ --work-tree=/mnt/c/Users/thomas"
 alias nixdot="git --git-dir=/etc/nixos/git/ --work-tree=/etc/nixos/"
