580ef29fecd436ecfe070c5c6a0e056bc12e2d4b
Thomas Heartman <thomas.heartman+git@gmail.com>
2020-08-21 09:23:55 +0200

    Delete customization from init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,19 +16,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (elisp-autojump eshell-autojump prettier-js rjsx-mode ripgrep general aggressive-indent persp-projectile persp-mode projectile feebleline multi-term origami elfeed-org notmuch editorconfig smartparens change-inner yasnippet ob-http org-ref expand-region link-hint edit-indirect multiple-cursors json-mode nix-mode direnv flycheck-rust rust-mode dap-mode lsp-treemacs lsp-ivy lsp-ui lsp-mode which-key rainbow-delimiters magit company-flx company-posframe company flycheck counsel one-themes use-package exwm)))
- '(sp-base-key-bindings (quote sp)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)
