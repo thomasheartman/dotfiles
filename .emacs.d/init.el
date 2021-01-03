008712b20d851022fdc4b76828620102ef43672b
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-01-03 13:17:20 +0100

    Move straight.el setup into init.el
    This helps me work around an issue relating to org mode (and
    org-agenda in particular). It has to be in init.el because org mode
    must be configured before using it (through tangling the org config).

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,28 @@
 
 (message "Using config dir: %s" my-config-dir)
 
+(setq straight-use-package-by-default t
+      straight-fix-flycheck t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; fix errors regarding org mode versions
+;; see https://github.com/raxod502/straight.el#the-wrong-version-of-my-package-was-loaded
+(straight-use-package 'org-plus-contrib)
+
+(message "Configured straight.el")
+
 (org-babel-load-file (expand-file-name "config.org" my-config-dir))
 
 (provide 'init)
