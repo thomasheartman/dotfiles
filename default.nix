99aeef88902805c49fef4b3c7c70035c2a2f8186
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-01-11 20:18:23 +0100

    Remove the manifest which stops imperative nix pkg install.
    This is necessary to make home-manager work.

--- default.nix
+++ default.nix
@@ -52,18 +52,19 @@ in buildEnv {
       nix-env --set -f ~ --argstr name "$(whoami)-user-env-$(date -I)"
     '')
 
-    # Manifest to make sure imperative nix-env doesn't work (otherwise it will overwrite the profile, removing all packages other than the newly-installed one).
-    (writeTextFile {
-      name = "break-nix-env-manifest";
-      destination = "/manifest.nix";
-      text = ''
-        throw '''
-          Your user environment is a buildEnv which is incompatible with
-          nix-env's built-in env builder. Edit your home expression and run
-          update-profile instead!
-        '''
-      '';
-    })
+    # # Manifest to make sure imperative nix-env doesn't work (otherwise it will overwrite the profile, removing all packages other than the newly-installed one).
+    # (writeTextFile {
+    #   name = "break-nix-env-manifest";
+    #   destination = "/manifest.nix";
+    #   text = ''
+    #     throw '''
+    #       Your user environment is a buildEnv which is incompatible with
+    #       nix-env's built-in env builder. Edit your home expression and run
+    #       update-profile instead!
+    #     '''
+    #   '';
+    # })
+
     # To allow easily seeing which nixpkgs version the profile was built from, place the version string in ~/.nix-profile/nixpkgs-version
     (writeTextFile {
       name = "nixpkgs-version";
