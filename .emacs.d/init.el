ac803f63162e4449ff396f8531d15942a5e7023d
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-01-12 22:27:45 +0100

    Hack around issues with straight and org.
    The hack uses fixes from raxod502's (straight author) init file
    system. Current version is [available here](https://github.com/raxod502/radian/blob/afe2882e3eb85c3284d90fd374be4a5ef9c8775a/radian-emacs/radian-org.el)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,11 +31,50 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; fix errors regarding org mode versions
-;; see https://github.com/raxod502/straight.el#the-wrong-version-of-my-package-was-loaded
-;; (straight-use-package 'org-plus-contrib)
+(straight-use-package 'use-package)
 
-(straight-use-package '(org :type built-in))
+(use-package git)
+(use-package org
+  :init
+
+  ;; The following is a temporary hack until straight.el supports
+  ;; building Org, see:
+  ;;
+  ;; * https://github.com/raxod502/straight.el/issues/211
+  ;; * https://github.com/raxod502/radian/issues/410
+  ;;
+  ;; There are three things missing from our version of Org: the
+  ;; functions `org-git-version' and `org-release', and the feature
+  ;; `org-version'. We provide all three of those ourself, therefore.
+
+  (defun org-git-version ()
+    "The Git version of org-mode.
+  Inserted by installing org-mode or when a release is made."
+    (require 'git)
+    (let ((git-repo (expand-file-name
+                     "straight/repos/org/" user-emacs-directory)))
+      (string-trim
+       (git-run "describe"
+                "--match=release\*"
+                "--abbrev=6"
+                "HEAD"))))
+
+  (defun org-release ()
+    "The release version of org-mode.
+  Inserted by installing org-mode or when a release is made."
+    (require 'git)
+    (let ((git-repo (expand-file-name
+                     "straight/repos/org/" user-emacs-directory)))
+      (string-trim
+       (string-remove-prefix
+        "release_"
+        (git-run "describe"
+                 "--match=release\*"
+                 "--abbrev=0"
+                 "HEAD")))))
+
+  (provide 'org-version))
+
 
 (message "Configured straight.el")
 
