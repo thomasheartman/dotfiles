899b76f1a4ac8fbac16f59d228f24e21346aed7c
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-02-09 20:09:21 +0100

    Add nixos config dir.
    First attempt at multi system config.

---
+++ nixos/base.nix
@@ -0,0 +1,256 @@
+{ config, pkgs, ... }:
+
+let
+  # keyboard
+  compiledLayout = pkgs.runCommand "keyboard-layout" { } ''
+    ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${/etc/nixos/layout.xkb} $out
+  '';
+
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+
+  unstableTarball = fetchTarball
+    "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz";
+
+
+in {
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelParams = [
+      "acpi_rev_override"
+      "mem_sleep_default=deep"
+    ];
+
+    tmpOnTmpfs = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  system.autoUpgrade.enable = true;
+
+  networking.networkmanager.enable = true;
+  networking.firewall = {
+    allowedTCPPorts = [ 17500 ];
+    allowedUDPPorts = [ 17500 ];
+  };
+
+  # Select internationalisation properties.
+  i18n ={  defaultLocale = "en_US.UTF-8"; };
+
+  console = {
+    useXkbConfig = true;
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  };
+
+  fonts = {
+    fonts = with pkgs; [
+      dejavu_fonts
+      fira-code
+      fira-code-symbols
+      ipafont
+      joypixels
+      kochi-substitute
+      mplus-outline-fonts
+      noto-fonts
+      powerline-fonts
+      symbola
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "DejaVu Sans Mono" "IPAGothic" ];
+        sansSerif = [ "DejaVu Sans" "IPAPGothic" ];
+        serif = [ "DejaVu Serif" "IPAPMincho" ];
+        emoji = [ "JoyPixels" ];
+      };
+    };
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball { config = config.nixpkgs.config; };
+    };
+
+    allowUnfree = true;
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      acpi
+      any-nix-shell
+      arandr
+      autorandr
+      bind
+      bitwarden-cli
+      cacert
+      cachix
+      curl
+      dropbox-cli
+      exfat
+      exfat-utils
+      fd
+      fish
+      git
+      home-manager
+      ispell
+      killall
+      libinput
+      libinput-gestures
+      networkmanager
+      notmuch
+      nvidia-offload
+      nvtop
+      pciutils
+      powertop
+      tmux
+      unzip
+      vim
+      thunderbolt
+      tree
+      wget
+      xcape
+      xclip
+      xorg.xev
+      xorg.xkbcomp
+      zip
+    ];
+
+    interactiveShellInit = ''
+      alias nixdot='git --git-dir=/etc/nixos/git --work-tree=/etc/nixos/'
+    '';
+
+    variables."SSL_CERT_FILE" = "/etc/ssl/certs/ca-bundle.crt";
+  };
+
+  # Enable sound.
+  sound.enable = true;
+
+  hardware.pulseaudio = {
+    enable = true;
+    support32Bit = true;
+    package = pkgs.pulseaudioFull;
+  };
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # uncomment this when moving to unstable or to stable after 21.03/05
+  # hardware.keyboard.zsa.enable = true;
+
+  programs = {
+    fish = {
+      enable = true;
+      promptInit = ''
+        any-nix-shell fish --info-right | source
+      '';
+    };
+    ssh.startAgent = true;
+    tmux = {
+      enable = true;
+      clock24 = true;
+    };
+    gnupg = { agent.enable = true; };
+  };
+
+  hardware.nvidiaOptimus.disable = true;
+
+  services.thermald.enable = true;
+
+  location.longitude = 10.45;
+  location.latitude = 59.54;
+
+  services.redshift = {
+    enable = true;
+    brightness.night = "0.8";
+    extraOptions = [ "-m randr" ];
+  };
+
+  # https://github.com/target/lorri/
+  services.lorri.enable = true;
+
+  services.xserver = {
+    enable = true;
+    libinput = {
+      enable = true;
+      naturalScrolling = true;
+      disableWhileTyping = true;
+    };
+
+    exportConfiguration = true;
+    autoRepeatDelay = 250;
+    autoRepeatInterval = 150;
+
+    videoDrivers = [ "intel" ];
+
+    windowManager.exwm = {
+      enable = true;
+      enableDefaultConfig = false;
+      extraPackages = epkgs: [
+        epkgs.emacsql-sqlite
+        epkgs.vterm
+        epkgs.magit
+        epkgs.pdf-tools
+      ];
+    };
+
+    useGlamor = true;
+
+    displayManager.autoLogin.user = "thomas";
+    displayManager.defaultSession = "none+exwm";
+  };
+
+  # users
+  users.extraUsers.thomas = {
+    name = "thomas";
+    group = "users";
+    extraGroups = [
+      "audio"
+      "disk"
+      "docker"
+      "networkmanager"
+      "plugdev"
+      "root"
+      "systemd-journal"
+      "video"
+      "wheel"
+    ];
+    createHome = true;
+    uid = 1000;
+    home = "/home/thomas";
+    shell = pkgs.fish;
+  };
+
+  nix.trustedUsers = [ "root" "thomas" ];
+  # services.emacs.enable = true;
+  services.emacs.defaultEditor = true;
+
+  services.offlineimap = {
+    enable = true;
+    install = true;
+    path = [ pkgs.notmuch pkgs.bitwarden-cli pkgs.fish ];
+  };
+
+  virtualisation.docker = {
+    enable = true;
+    autoPrune = {
+      enable = true;
+      flags = [ "--all" ];
+    };
+  };
+}
---
+++ nixos/hosts/xps/configuration.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../base.nix
+  ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+  services.udev.extraRules = ''
+    # Teensy rules for the Ergodox EZ Original / Shine / Glow
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+    KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+    # STM32 rules for the Planck EZ Standard / Glow
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
+        MODE:="0666", \
+        SYMLINK+="stm32_dfu"
+
+    # Runtime PM for PCI Device NVIDIA Corporation GP107M [GeForce GTX 1050 Ti Mobile]
+    ACTION=="add", SUBSYSTEMS=="pci", ATTRS{device}=="0x1901", ATTRS{vendor}=="0x8086", TEST=="power/control", ATTR{power/control}="auto"
+  '';
+
+  # based on https://nixos.wiki/wiki/Dropbox and https://discourse.nixos.org/t/using-dropbox-on-nixos/387/5
+  systemd.user.services.dropbox = {
+    description = "Dropbox";
+    wantedBy = [ "graphical-session.target" ];
+    environment = {
+      QT_PLUGIN_PATH = "/run/current-system/sw/"
+        + pkgs.qt5.qtbase.qtPluginPrefix;
+      QML2_IMPORT_PATH = "/run/current-system/sw/"
+        + pkgs.qt5.qtbase.qtQmlPrefix;
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+      ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+      KillMode = "control-group"; # upstream recommends process
+      Restart = "on-failure";
+      PrivateTmp = true;
+      ProtectSystem = "full";
+      Nice = 10;
+    };
+  };
+  security.sudo.extraConfig = ''
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/poweroff
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/reboot
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/systemctl suspend
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/systemctl hibernate
+  '';
+
+  systemd.user.services.autorandrize = {
+    enable = true;
+    description = "Automatically adjust screens when waking up";
+    wantedBy = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
+    after = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      TimeOutSec = "0";
+      ExecStart = "${pkgs.autorandr}/bin/autorandr -c";
+    };
+  };
+}
