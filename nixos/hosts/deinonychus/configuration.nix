c9aaeaadc721cddcae914358c89f4b1a2dd450cc
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-02-10 16:23:18 +0100

    Add deinonychus dir

---
+++ nixos/hosts/deinonychus/configuration.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    <nixos-hardware/dell/xps/15-7590>
+    ./hardware-configuration.nix
+    ./../../base.nix
+  ];
+
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+  };
+
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking = {
+    useDHCP = false;
+    interfaces = {
+      wlp59s0.useDHCP = true;
+      ens1.useDHCP = true;
+    };
+    hostname = "deinonychus";
+  };
+
+  programs.xss-lock = {
+    enable = true;
+    lockerCommand = ''
+      ${pkgs.i3lock}/bin/i3lock -c 000000
+    '';
+  };
+
+  services.logind.extraConfig = ''
+    IdleAction=suspend-then-hibernate
+    IdleActionSec=1800
+  '';
+
+  services.hardware.bolt.enable = true;
+
+  security.pam.enableEcryptfs = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+}
