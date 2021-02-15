afd3b3dde9399c38d6e36ac01f2f8fef7a32dfc1
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-02-15 16:04:47 +0100

    Add virtualbox for Deinonychus.

--- nixos/hosts/deinonychus/configuration.nix
+++ nixos/hosts/deinonychus/configuration.nix
@@ -43,6 +43,12 @@
 
   security.pam.enableEcryptfs = true;
 
+  virtualisation.virtualbox.host = {
+    enable = true;
+  };
+
+  users.extraUsers.thomas.extraGroups = [ "vboxusers" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
