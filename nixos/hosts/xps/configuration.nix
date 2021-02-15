1785b768c1ad1a06b1bc5d4a01d45ced7987c756
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-02-15 20:29:13 +0100

    Add hostname for home xps Archaeopteryx.

--- nixos/hosts/xps/configuration.nix
+++ nixos/hosts/xps/configuration.nix
@@ -15,6 +15,8 @@
     displayManager.autoLogin.user = "thomas";
   };
 
+  networking.hostName = "archaeopteryx";
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
