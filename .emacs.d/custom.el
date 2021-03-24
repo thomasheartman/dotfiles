39275d6be55175643a94a2f1ab7d7f15c69462e9
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-03-24 09:29:08 +0100

    Remove notmuch config from custom.el.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,14 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(notmuch-saved-searches
-   '((:name "inbox" :query "tag:inbox" :key "i")
-     (:name "unread" :query "tag:unread" :key "u")
-     (:name "flagged" :query "tag:flagged" :key "f")
-     (:name "sent" :query "tag:sent" :key "t")
-     (:name "drafts" :query "tag:draft" :key "d")
-     (:name "all mail" :query "*" :key "a")
-     (:name "enonic" :query "to:the@enonic"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
