552fa2499d24e3edd3aa9ea68626dfb26378b88d
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-06-02 14:28:35 +0200

    Enable PipeWire for audio.

--- nixos/base.nix
+++ nixos/base.nix
@@ -154,13 +154,39 @@ in
     variables."SSL_CERT_FILE" = "/etc/ssl/certs/ca-bundle.crt";
   };
 
-  # Enable sound.
-  sound.enable = true;
-
-  hardware.pulseaudio = {
+  # audio config based on https://nixos.wiki/wiki/PipeWire
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    support32Bit = true;
-    package = pkgs.pulseaudioFull;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+
+    media-session.config.bluez-monitor.rules = [
+      {
+        # Matches all cards
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+            # mSBC is not expected to work on all headset + adapter combinations.
+            "bluez5.msbc-support" = true;
+          };
+        };
+      }
+      {
+        matches = [
+          # Matches all sources
+          { "node.name" = "~bluez_input.*"; }
+          # Matches all outputs
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+    ];
   };
 
   hardware.bluetooth.enable = true;
