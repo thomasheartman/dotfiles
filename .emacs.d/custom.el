357b9cafc8fc762a2bc86106ce397ff3e2a314e3
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-07-08 07:52:06 +0200

    Remove the `scroll-conservatively` config from custom.el
    It's already customised in config.org

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scroll-conservatively 2))
+ '(warning-suppress-types '((use-package) (:warning))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
