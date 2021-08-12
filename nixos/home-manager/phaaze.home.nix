213c849430542595b74b1f958bd71114f7ff4478
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-08-12 20:33:16 +0200

    Add home.nix for phaaze.

---
+++ nixos/home-manager/phaaze.home.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./home.nix ];
+
+  # note: this is to make the hostname available to home-manager.
+  # Ideally I wouldn't have to manually repeat it here after declaring
+  # it in the nix configuration, but to share the variable would
+  # require some more work. See
+  # https://www.lafuente.me/posts/installing-home-manager/ for more
+  # info.
+  home.sessionVariables = {
+    HOSTNAME = "phaaze";
+  };
+}
