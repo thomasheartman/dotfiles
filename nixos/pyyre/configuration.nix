17634588801564fc1c8b9a16ba76c6d40ecaf1ad
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-09-02 14:06:39 +0200

    Add pyyre config files.

---
+++ nixos/pyyre/configuration.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+let
+
+  hostname = baseNameOf ./.;
+
+in
+{
+  imports = [
+    <nixos-hardware/lenovo/thinkpad>
+    <nixos-hardware/lenovo/thinkpad/yoga.nix>
+    ./hardware-configuration.nix
+    ../base.nix
+  ];
+
+  services.xserver = {
+    useGlamor = true;
+    displayManager.autoLogin.user = "thomas";
+  };
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking = {
+    useDHCP = false;
+    interfaces.wlp0s20f3.useDHCP = true;
+    hostName = hostname;
+  };
+
+  services.logind.extraConfig = ''
+    IdleAction=suspend-then-hibernate
+    IdleActionSec=1800
+  '';
+
+  security.pam.enableEcryptfs = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+  };
+
+  services.fprintd.enable = true;
+}
