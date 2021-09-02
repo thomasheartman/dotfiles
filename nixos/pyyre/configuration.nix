0b53bae44fc97adc030adabe0fa63ce055d4748c
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-09-02 20:31:04 +0200

    Add fwupd as a service.

--- nixos/pyyre/configuration.nix
+++ nixos/pyyre/configuration.nix
@@ -33,6 +33,8 @@ in
     '';
   };
 
+  services.fwupd.enable = true;
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
