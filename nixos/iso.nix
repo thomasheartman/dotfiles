7c8979c4263b5b38935e1f09433c4da3d6443877
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-09-02 12:09:17 +0200

    Add more packages to iso.nix.

--- nixos/iso.nix
+++ nixos/iso.nix
@@ -1,18 +1,54 @@
 # Use this file to create custom ISO images (as detailed in
 # https://blog.thomasheartman.com/posts/building-a-custom-nixos-installer)
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
 
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+
   # programs that should be available in the installer
   environment.systemPackages = with pkgs; [
+    acpi
+    any-nix-shell
+    arandr
+    autorandr
+    bind
+    bitwarden-cli
+    cacert
+    cachix
+    cryptsetup
+    curl
+    exfat
+    exfat-utils
+    fd
     fish
     git
-    cryptsetup
     home-manager
-    curl
+    ispell
+    killall
+    libinput
+    libinput-gestures
+    networkmanager
+    notmuch
+    nvtop
+    pciutils
+    powertop
+    thunderbolt
+    tmux
+    tree
+    unzip
+    vim
+    wget
+    xcape
+    xclip
+    xorg.xev
+    xorg.xkbcomp
+    zip
   ];
 }
