47ec76b44664fa7deebbb3cd6821f65d99f0ae63
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-09-02 14:05:57 +0200

    Move NVIDIA-config into phaaze config file.

--- nixos/base.nix
+++ nixos/base.nix
@@ -6,13 +6,6 @@ let
     ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${/etc/nixos/layout.xkb} $out
   '';
 
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
 
   unstableTarball = fetchTarball
     "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz";
@@ -28,12 +21,8 @@ in
     loader.efi.canTouchEfiVariables = true;
     kernelParams = [
       "mem_sleep_default=deep"
-      "acpi_rev_override"
-      "intel_iommu=igfx=off"
-      "nvidia-drm.modeset=1"
     ];
     kernelPackages = pkgs.linuxPackages_5_4;
-    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
     tmpOnTmpfs = true;
   };
@@ -117,8 +106,6 @@ in
       libinput-gestures
       networkmanager
       notmuch
-      nvidia-offload
-      nvtop
       pciutils
       powertop
       tmux
@@ -215,13 +202,6 @@ in
     gnupg = { agent.enable = true; };
   };
 
-  hardware.nvidia.prime = {
-    sync.enable = true;
-
-    nvidiaBusId = "PCI:1:0:0";
-    intelBusId = "PCI:0:2:0";
-  };
-
   services.thermald.enable = true;
 
   location.longitude = 10.45;
@@ -252,34 +232,6 @@ in
     exportConfiguration = true;
     autoRepeatDelay = 250;
     autoRepeatInterval = 150;
-
-    videoDrivers = [ "nvidia" ];
-
-    config = ''
-      Section "Device"
-          Identifier  "Intel Graphics"
-          Driver      "intel"
-          #Option      "AccelMethod"  "sna" # default
-          #Option      "AccelMethod"  "uxa" # fallback
-          Option      "TearFree"        "true"
-          Option      "SwapbuffersWait" "true"
-          BusID       "PCI:0:2:0"
-          #Option      "DRI" "2"             # DRI3 is now default
-      EndSection
-
-      Section "Device"
-          Identifier "nvidia"
-          Driver "nvidia"
-          BusID "PCI:1:0:0"
-          Option "AllowEmptyInitialConfiguration"
-      EndSection
-    '';
-
-    screenSection = ''
-      Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
-      Option         "AllowIndirectGLXProtocol" "off"
-      Option         "TripleBuffer" "on"
-    '';
 
     windowManager.exwm = {
       enable = true;
--- nixos/phaaze.nix
+++ nixos/phaaze.nix
@@ -8,7 +8,18 @@ in
 {
   imports = [ (./. + "/${hostname}.hardware-configuration.nix") ./base.nix ];
 
-  boot.kernelParams = [ "acpi_rev_override" ];
+  boot = {
+    kernelParams = [
+      "acpi_rev_override"
+      "intel_iommu=igfx=off"
+      "nvidia-drm.modeset=1"
+    ];
+    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  };
+
+  environment.systemPackages = [
+    pkgs.nvtop
+  ];
 
   services.xserver = {
     useGlamor = true;
@@ -18,7 +29,41 @@ in
 
   networking.hostName = hostname;
 
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
 
+    config = ''
+      Section "Device"
+          Identifier  "Intel Graphics"
+          Driver      "intel"
+          #Option      "AccelMethod"  "sna" # default
+          #Option      "AccelMethod"  "uxa" # fallback
+          Option      "TearFree"        "true"
+          Option      "SwapbuffersWait" "true"
+          BusID       "PCI:0:2:0"
+          #Option      "DRI" "2"             # DRI3 is now default
+      EndSection
+
+      Section "Device"
+          Identifier "nvidia"
+          Driver "nvidia"
+          BusID "PCI:1:0:0"
+          Option "AllowEmptyInitialConfiguration"
+      EndSection
+    '';
+
+    screenSection = ''
+      Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+      Option         "AllowIndirectGLXProtocol" "off"
+      Option         "TripleBuffer" "on"
+    '';
+  };
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    nvidiaBusId = "PCI:1:0:0";
+    intelBusId = "PCI:0:2:0";
+  };
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
