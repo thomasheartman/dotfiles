194316e4f0d5a48184b214b3213fff30f8354d6c
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-10-03 22:47:27 +0200

    Config i3 from home-manager (including key binds, status bar)

--- nixos/home.nix
+++ nixos/home.nix
@@ -2,6 +2,8 @@
 
 let
 
+ mod = "Mod4";
+
   unstable = import (
     fetchTarball
       "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz"
@@ -204,14 +206,53 @@ in
   #     };
 
   xsession = {
-    enable = false;
-    # windowManager.i3.enable = true;
+    enable = true;
     # windowManager.command = ''
     #   ${config.programs.emacs.package}/bin/emacs -l "${exwm-load-script}"
     # '';
     initExtra = ''
       xset r rate 200 100
     '';
+
+    windowManager.i3 = {
+      enable = true;
+      config = {
+        modifier = mod;
+
+        fonts = [ "DejaVu Sans Mono, FontAwesome 20" ];
+
+        keybindings = pkgs.lib.mkOptionDefault {
+          "${mod}+space" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+          "${mod}+x" = "exec sh -c '${pkgs.maim}/bin/maim -s | xclip -selection clipboard -t image/png'";
+          "${mod}+Shift+x" = "exec sh -c '${pkgs.i3lock}/bin/i3lock -c 222222 & sleep 5 && xset dpms force of'";
+
+          # Focus
+          "${mod}+j" = "focus left";
+          "${mod}+k" = "focus down";
+          "${mod}+l" = "focus up";
+          "${mod}+semicolon" = "focus right";
+
+          # Move
+          "${mod}+Shift+j" = "move left";
+          "${mod}+Shift+k" = "move down";
+          "${mod}+Shift+l" = "move up";
+          "${mod}+Shift+semicolon" = "move right";
+
+          # My multi monitor setup
+          "${mod}+m" = "move workspace to output DP-2";
+          "${mod}+Shift+m" = "move workspace to output DP-5";
+        };
+
+
+        bars = [
+          {
+            position = "bottom";
+            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${./i3status-rust.toml}";
+          }
+        ];
+      };
+    };
+
   };
 
   home.keyboard = {
