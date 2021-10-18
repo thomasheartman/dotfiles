a8d166473404d13a80d2736021f532278ec3fc1c
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-10-18 07:22:07 +0200

    Experimental: remove i3 from base.nix 🤞
    On first glance, at least, this seems to work with i3 in home-manager.

--- nixos/base.nix
+++ nixos/base.nix
@@ -310,18 +310,7 @@ in
       noDesktop = true;
     };
 
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-        i3blocks
-      ];
-    };
-
-    displayManager.defaultSession = "xfce+i3";
+    displayManager.defaultSession = "xfce";
   };
 
   # users
