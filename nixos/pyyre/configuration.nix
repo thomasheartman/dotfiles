30e745cfbcda1e254cdbedad8338950dacdb90c1
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-10-18 07:45:52 +0200

    Fix pyyre performance problems? But reintroduce screen tearing.
    As suggested in this thread
    (https://discourse.nixos.org/t/eliminate-screen-tearing-with-intel-mesa/14724/20),
    changing the video drivers section seems to have worked.

--- nixos/pyyre/configuration.nix
+++ nixos/pyyre/configuration.nix
@@ -26,11 +26,11 @@ in
   services.xserver = {
     useGlamor = true;
     displayManager.autoLogin.user = "thomas";
-    videoDrivers = [ "intel" ];
-    deviceSection = ''
-      Option "DRI" "2"
-      Option "TearFree" "true"
-    '';
+    videoDrivers = [ "modesetting" ];
+    # deviceSection = ''
+    #   Option "DRI" "2"
+    #   Option "TearFree" "true"
+    # '';
   };
 
   services.fwupd.enable = true;
