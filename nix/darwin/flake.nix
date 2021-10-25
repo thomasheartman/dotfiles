ecd6fb3d45a4407cc8e1eb5d431b81578590dbe8
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-10-25 14:36:52 +0200

    Update host name in flake.nix

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -9,7 +9,7 @@
   outputs = { mk-darwin-system, ... }@inputs:
     let
       flake-utils = mk-darwin-system.inputs.flake-utils;
-      hostName = "example";
+      hostName = "termina";
       systems = [ "aarch64-darwin" ];
     in flake-utils.lib.eachSystem systems (system:
       mk-darwin-system.mkDarwinSystem {
@@ -22,11 +22,11 @@
             nix.package = pkgs.nixFlakes;
             nix.extraOptions = ''
               system = aarch64-darwin
-              extra-platforms = aarch64-darwin x86_64-darwin 
+              extra-platforms = aarch64-darwin x86_64-darwin
               experimental-features = nix-command flakes
               build-users-group = nixbld
             '';
-            environment.systemPackages = with pkgs; [ nixFlakes home-manager ];
+            environment.systemPackages = with pkgs; [ nixFlakes home-manager vim emacs git fish autojump bat bitwarden-cli direnv ispell jetbrains-mono jq nixfmt rnix-lsp sd slack victor-mono wally-cli watchexec yaml-language-server yabai];
           })
         ];
 
