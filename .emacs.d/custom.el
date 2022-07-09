d2ba395c3bb5550c1f15720ba3668131932b14b7
Thomas Heartman <thomas.heartman+git@gmail.com>
2022-07-09 20:10:23 +0200

    Abbrevs and dicts

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,27 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(safe-local-variable-values
+   '((org-d20-party
+      ("Zahrat" . 2)
+      ("Ennon" . 4)
+      ("Artemis" . 5))))
+ '(warning-suppress-log-types
+   '(((undo discard-info))
+     ((undo discard-info))
+     ((undo discard-info))
+     ((undo discard-info))
+     (comp)
+     (use-package)
+     (:warning)))
+ '(warning-suppress-types
+   '(((undo discard-info))
+     ((undo discard-info))
+     ((undo discard-info))
+     ((undo discard-info))
+     (comp)
+     (use-package)
+     (:warning)) nil nil "Customized with use-package emacs"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
