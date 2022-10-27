1a51de733874ee5d29ee185ce2af2f3b1698dc9d
Thomas Heartman <thomas.heartman+git@gmail.com>
2022-10-27 16:02:01 +0200

    Update darwin files

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -1,85 +1,74 @@
 {
-  description = "Minimal DarwinSystem example";
+  description = "Minimal mkDarwinSystem example";
 
   inputs = {
-    # in real world use: ""
+    # change tag or commit of nixpkgs for your system
+    nixpkgs.url = "github:nixos/nixpkgs/21.11";
+
+    # change main to a tag o git revision
     mk-darwin-system.url = "github:vic/mk-darwin-system/main";
+    mk-darwin-system.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { mk-darwin-system, ... }@inputs:
+  outputs = { self, nixpkgs, mk-darwin-system, ... }:
     let
-      flake-utils = mk-darwin-system.inputs.flake-utils;
-      hostName = "termina";
-      systems = [ "aarch64-darwin" ];
-    in
-      flake-utils.lib.eachSystem systems (
-        system:
-          mk-darwin-system.mkDarwinSystem {
-            inherit hostName system;
+      darwinFlakeOutput = mk-darwin-system.mkDarwinSystem.m1 {
 
-            nixosModules = [
-              (
-                { pkgs, ... }: {
-                  system.stateVersion = 4;
-                  services.nix-daemon.enable = true;
-                  nix.package = pkgs.nixFlakes;
-                  nix.extraOptions = ''
-                    system = aarch64-darwin
-                    extra-platforms = aarch64-darwin x86_64-darwin
-                    experimental-features = nix-command flakes
-                    build-users-group = nixbld
-                  '';
+        # Provide your nix modules to enable configurations on your system.
+        #
+        modules = [
+          # You might want to split them into separate files
+          #  ./modules/host-one-module.nix
+          #  ./modules/user-one-module.nix
+          #  ./modules/user-two-module.nix
+          # Or you can inline them here, eg.
 
-                  networking.hostName = hostName;
+          # for configurable nix-darwin modules see
+          # https://github.com/LnL7/nix-darwin/blob/master/modules/module-list.nix
+          ({ config, pkgs, ... }: {
+            environment.systemPackages = with pkgs; [ ];
+          })
 
-                  fonts = {
-                    fonts = with pkgs; [
-                      dejavu_fonts
-                      fira-code
-                      fira-code-symbols
-                      font-awesome
-                      ipafont
-                      kochi-substitute
-                      mplus-outline-fonts
-                      nerdfonts
-                      noto-fonts
-                      # noto-fonts-emoji # <- doesn't work because of scipy
-                      open-sans
-                      powerline-fonts
-                      siji
-                      symbola
-                    ];
-                  };
+          # for configurable home-manager modules see:
+          # https://github.com/nix-community/home-manager/blob/master/modules/modules.nix
+          {
+            home-manager = {
+              # sharedModules = []; # per-user modules.
+              # extraSpecialArgs = {}; # pass aditional arguments to all modules.
+            };
+          }
 
-                  nixpkgs.config = {
-                    allowUnfree = true;
-                  };
+          # An example of user environment. Change your username.
+          ({ pkgs, lib, ... }: {
+            home-manager.users."thomas" = {
 
-                  environment.systemPackages = with pkgs; [
+            home.packages = with pkgs; [
+
                     alacritty
                     (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
                     autojump
                     bat
                     bitwarden-cli
-                    cachix
+                    # cachix
                     direnv
                     emacs
-                    fish
+                    # fish
                     git
                     git-lfs
+                    gnupg
                     home-manager
                     ispell
                     jetbrains-mono
                     jq
-                    nixFlakes
-                    nixfmt
+                    # nixFlakes
+                    # nixfmt
                     rnix-lsp
                     ripgrep
                     sd
                     victor-mono
                     vim
                     wally-cli
-                    watchexec
+                    # watchexec
                     yaml-language-server
                     # yabai <— fails for some reason
 
@@ -89,14 +78,42 @@
                         open -na Firefox.app --args --P default-release
                       ''
                     )
-                  ];
-                }
-              )
+
             ];
 
-            flakeOutputs = { pkgs, ... }@outputs:
-              outputs // (with pkgs; { packages = { inherit hello; }; });
+              # enable at least one shell. as for any other program, see customizable options at:
+              # https://github.com/nix-community/home-manager/blob/master/modules/programs/<program>.nix
+              programs.zsh.enable = true;
+              # programs.fish.enable = true;
+              # programs.bash.enable = true;
 
-          }
-      );
+              # create some custom dot-files on your user's home.
+              home.file.".config/foo".text = "bar";
+
+              programs.git = {
+                enable = true;
+                userName = "Thomas Heartman";
+                userEmail = "thomas@getunleash.ai";
+              };
+            };
+          })
+
+          # for configurable nixos modules see (note that many of them might be linux-only):
+          # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/module-list.nix
+          ({ config, lib, ... }: {
+            # You can provide an overlay for packages not available or that fail to compile on arm.
+            # nixpkgs.overlays =
+              #[ (self: super: { inherit (lib.mds.intelPkgs) pandoc niv; }) ];
+
+            # You can enable supported services (if they work on arm and are not linux only)
+            #services.lorri.enable = true;
+          })
+
+        ];
+      };
+    in darwinFlakeOutput // {
+      # Your custom flake output here.
+      darwinConfigurations."termina" =
+        darwinFlakeOutput.darwinConfiguration.aarch64-darwin;
+    };
 }
