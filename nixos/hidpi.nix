e631ed17e2b8209d5d6060c57e32de63e630d30a
Thomas Heartman <thomas.heartman+git@gmail.com>
2022-12-13 15:23:44 +0100

    Parameterize hidpi module

--- nixos/hidpi.nix
+++ nixos/hidpi.nix
@@ -1,13 +1,17 @@
 # hidpi support config variables
-{ pkgs, ... }:
+{ pkgs, scale ? 2, ... }:
+
+let dpiScale = 1.0 / scale;
+
+in
 
 {
   services.xserver.dpi = 180;
   console.font =
     "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
   environment.variables = {
-    GDK_SCALE = "2";
-    GDK_DPI_SCALE = "0.5";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+    GDK_SCALE = toString scale;
+    GDK_DPI_SCALE = pkgs.lib.strings.floatToString dpiScale;
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=${toString scale}";
   };
 }
