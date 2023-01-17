3b3e8673c7f1ea8aef1714be20a1ba75bf0bdda1
Thomas Heartman <thomas.heartman+git@gmail.com>
2023-01-17 22:50:31 +0100

    Update skytown config

--- nixos/skytown/configuration.nix
+++ nixos/skytown/configuration.nix
@@ -4,44 +4,20 @@
 
 { config, pkgs, ... }:
 
-let 
+let
 
-hostname = baseNameOf ./.;
+  hostname = baseNameOf ./.;
 
 in
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
-../base.nix
+      ../base.nix
+      ../steam.nix
     ];
-  
-networking.hostName = hostname;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -51,5 +27,15 @@ networking.hostName = hostname;
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
-}
+  networking.hostName = hostname;
 
+  hardware.video.hidpi.enable = true;
+
+  # musnix https://github.com/musnix/musnix
+  musnix = {
+    enable = true;
+    kernel = {
+      realtime = true;
+    };
+  };
+}
