6e6f8bf9c2c66870e9fad3bfc37878bea4c11f0a
Thomas Heartman <thomas.heartman+git@gmail.com>
2023-03-13 19:51:40 +0100

    Picom: fmt and disable shadows for popup menus

--- nixos/picom.nix
+++ nixos/picom.nix
@@ -1,45 +1,45 @@
 { pkgs, config, ... }:
 
+let theme = import ./theme.nix;
 
-let
-  theme = import ./theme.nix;
+in {
+  services.picom = let shadowRadius = 8;
+  in {
+    enable = true;
+    shadow = true;
 
-in
-{
-  services.picom = let shadowRadius = 8; in
-    {
-      enable = true;
-      shadow = true;
+    settings = {
 
+      inactive-dim = "0.1";
 
-      settings = {
+      shadow-exclude = [ "window_type = 'dock'" ];
 
-        inactive-dim = "0.1";
-
-        shadow-exclude = [
-          "window_type = 'dock'"
-        ];
-
-        # Rounding corners doesn't work well with i3's child borders,
-        # so let's leave that out for now.
-
-        # Sets the radius of rounded window corners. When > 0, the
-        # compositor will round the corners of windows. Does not
-        # interact well with `transparent-clipping`.
-        # corner-radius = 15;
-
-        # Exclude conditions for rounded corners.
-        # rounded-corners-exclude = [
-        #   "window_type = 'dock'"
-        #   "window_type = 'desktop'"
-        #   "window_type = 'menu'"
-        #   "window_type = 'dnd'"
-        #   # to find a class, use `xprop` and check the
-        #   # WM_CLASS(STRING) property
-        #   "class_g = 'i3-frame'"
-        # ];
-
+      wintypes = {
+        dropdown_menu = { shadow = false; };
+        popup_menu = { shadow = false; };
+        utility = { shadow = false; };
       };
 
+      # Rounding corners doesn't work well with i3's child borders,
+      # so let's leave that out for now.
+
+      # Sets the radius of rounded window corners. When > 0, the
+      # compositor will round the corners of windows. Does not
+      # interact well with `transparent-clipping`.
+      # corner-radius = 15;
+
+      # Exclude conditions for rounded corners.
+      # rounded-corners-exclude = [
+      #   "window_type = 'dock'"
+      #   "window_type = 'desktop'"
+      #   "window_type = 'menu'"
+      #   "window_type = 'dnd'"
+      #   # to find a class, use `xprop` and check the
+      #   # WM_CLASS(STRING) property
+      #   "class_g = 'i3-frame'"
+      # ];
+
     };
+
+  };
 }
