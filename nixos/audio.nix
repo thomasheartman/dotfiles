8a3a9b006f681e76174b5b9ef16e6bd2fc12652a
Thomas Heartman <thomas.heartman+git@gmail.com>
2023-03-20 18:43:20 +0100

    Add new audio plugins

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -6,14 +6,21 @@
     artyFX
     bitwig-studio
     calf
+    ChowCentaur
+    ChowKick
+    ChowPhaser
+    CHOWTapeModel
     dexed
     distrho
     drumgizmo
+    eq10q
     guitarix
+    odin2
     reaper
     surge
-    # tonelib-gfx # <- this one often has problems :/
+    # tonelib-gfx # <- this one often has problems due to hash mismatches :/
     tap-plugins
+    tunefish
     zrythm
     (callPackage ./scarlett-mixer.nix { })
     (callPackage ./zebralette.nix { })
