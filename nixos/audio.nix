a0fae040d6907607f068268fa10523a023165b0f
Thomas Heartman <thomas.heartman+git@gmail.com>
2023-04-07 17:09:57 +0200

    Update audio packages

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -6,6 +6,7 @@
     artyFX
     bitwig-studio
     calf
+    carla
     ChowCentaur
     ChowKick
     ChowPhaser
@@ -16,13 +17,17 @@
     eq10q
     guitarix
     odin2
+    playonlinux # for managing wine installations
     reaper
     surge
     # tonelib-gfx # <- this one often has problems due to hash mismatches :/
     tap-plugins
     tunefish
+    yabridge
+    yabridgectl
     zrythm
     (callPackage ./scarlett-mixer.nix { })
+    (callPackage ./podolski.nix { })
     (callPackage ./zebralette.nix { })
   ];
 }
